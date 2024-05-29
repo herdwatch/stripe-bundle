fb4d5df27413774bc0f0d0d3cb823f527c700ed3
Vladimir Taskalin <{ID}+{username}@users.noreply.github.com>
2024-05-29 19:46:28 +0300

    fix

--- src/DependencyInjection/MiracodeStripeExtension.php
+++ src/DependencyInjection/MiracodeStripeExtension.php
@@ -24,7 +24,8 @@ class MiracodeStripeExtension extends Extension
         $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('actions.php');
         $loader->load('transformer.php');
-
+        $loader->load('services.xml');
+        
         $container->setParameter('miracode_stripe.secret_key', $config['secret_key']);
         $container->setParameter('miracode_stripe.process_service', 'miracode_stripe.default_handler');
 
